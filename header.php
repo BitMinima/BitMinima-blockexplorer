8ce189ea9d462b77528f8c2d32e041870beebc32
Denlon210 <denlon210@gmail.com>
2017-02-25 13:12:12 +0300

    Update header.php

--- header.php
+++ header.php
@@ -12,22 +12,22 @@ setcookie("lang", en);
 $lang = "en";
 }
 }
-$langconst = parse_ini_file("/lang/$lang.ini");
+$langconst = parse_ini_file("lang/$lang.ini");
 ?>
 <html lang="en">
 		<!-- scripts -->
-		<script src="/assets/js/jquery-3.1.0.min.js"></script>
-		<script src="/assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/jquery-3.1.0.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
 		<script src="http://canvasjs.com/assets/script/canvasjs.min.js"></script>
 <head>
 <!-- <meta charset="Windows-1251" /> -->
 <meta charset="charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title><?php echo $fullname ?> block explorer</title>
-<link href="/css/bootstrap.min.css" rel="stylesheet">
-<link href="/css/font-awesome-4.4.0/css/font-awesome.min.css" rel="stylesheet"/>
-<link href="/css/style.css" rel="stylesheet">
-<link rel="shortcut icon" href="/img/favicon.ico"/>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/font-awesome-4.4.0/css/font-awesome.min.css" rel="stylesheet"/>
+<link href="css/style.css" rel="stylesheet">
+<link rel="shortcut icon" href="img/favicon.ico"/>
 <meta name="theme-color" content="#ffffff">
 <script type="text/javascript">
 function reloadPage()
@@ -40,7 +40,7 @@ window.location.reload()
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/index.php"><?php echo $fullname ?> block explorer</a>
+      <a class="navbar-brand" href="index.php"><?php echo $fullname ?> block explorer</a>
     </div>
     <ul class="nav navbar-nav">
     </ul>
@@ -48,8 +48,8 @@ window.location.reload()
 <li class="dropdown">
 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $langconst['lng'] ?><span class="caret"></span></a>
 <ul class="dropdown-menu">
-<li><a href="?lang=en" onclick="reloadPage()" title="English"><img src="/img/flags/US.png"> English</a></li>
-<li><a href="?lang=ru" onclick="reloadPage()" title="Pусский"><img src="/img/flags/RU.png"> Pусский</a></li>
+<li><a href="?lang=en" onclick="reloadPage()" title="English"><img src="img/flags/US.png"> English</a></li>
+<li><a href="?lang=ru" onclick="reloadPage()" title="Pусский"><img src="img/flags/RU.png"> Pусский</a></li>
 </ul>
 </li>
 </ul>
